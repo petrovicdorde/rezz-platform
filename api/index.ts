10409d5615c33312b64e3a89f5042bd2bc47e141
djordje.petrovic <djordje.petrovic@vidinext.com>
2026-04-20 19:13:31 +0200

    fix: import from TS source in api/index.ts, remove dist dependency

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { configure as serverlessExpress } from '@vendia/serverless-express';
-import express from 'express';
-import type { IncomingMessage, ServerResponse } from 'http';
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { AppModule } = require('../apps/api/dist/app.module');
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { GlobalExceptionFilter } = require('../apps/api/dist/common/filters/http-exception.filter');
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import { configure as serverlessExpress } from "@vendia/serverless-express";
+import express from "express";
+import type { IncomingMessage, ServerResponse } from "http";
+import { AppModule } from "../apps/api/src/app.module";
+import { GlobalExceptionFilter } from "../apps/api/src/common/filters/http-exception.filter";
 
 let cachedServer: ReturnType<typeof serverlessExpress>;
 
@@ -16,14 +13,17 @@ async function bootstrap() {
   if (cachedServer) return cachedServer;
 
   const expressApp = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp)
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-    }),
+    })
   );
 
   app.useGlobalFilters(new GlobalExceptionFilter());
@@ -39,7 +39,10 @@ async function bootstrap() {
   return cachedServer;
 }
 
-export default async function handler(req: IncomingMessage, res: ServerResponse) {
+export default async function handler(
+  req: IncomingMessage,
+  res: ServerResponse
+) {
   const server = await bootstrap();
   return server(req, res, () => {});
 }
